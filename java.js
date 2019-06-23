289d1b2e74a2601047601a82a1b200c8019a7841
Jatin Agarwal <aggarwaljatin16@gmail.com>
2019-06-23 23:44:50 +0530

    no message

--- java.js
+++ java.js
@@ -5,7 +5,7 @@ function send() {
     
     messageobj = {
                  message: message,
-                 author: "Jatin"
+                 author: "Jatin"          // give it your name  ex: author: "your name"
               }
        write(messageobj)
        $("#message").val("")
@@ -19,15 +19,28 @@ function write(objmessage) {
 
 function read()
 {
-    database.ref('/chat').on('value',function (data){
+    database.ref('/chat').on('value',function (data){    //.on is the listener that is always checking any updation in databaseand tell the frontend as soon as changes are made
     
      let messages = data.val();
     $("#display").empty();
+    $("#friend").empty();
 
     for(messagekey in messages)
     {     
     	let message = messages[messagekey];
-    	$("#display").append(`<!-- <p id= "name">${message.author}</p> --> <p id="text">${message.message}</p><br>`)
+        let friendname = ""; 
+
+        if(message.author == "Jatin")
+    	{
+             $("#display").append(`<!-- <p id= "name">${message.author}</p> --> <p id="text">${message.message}</p><br>`)  //your message. This will be displayed on right side of screen 
+        }
+         
+        else
+        {
+            $("#display").append(`<!-- <p id= "name">${message.author}</p> --> <p id="text">${message.message}</p><br>`)   //your friend's message. This will be displayed on left side of screen
+             friendname = message.author;
+        }
+        document.getElementById("friend").innerHTML = friendname;
     }
 
     }); 
